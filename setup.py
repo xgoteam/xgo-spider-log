4aba8d5223e0d61f0f7a09bb4467a59357c611ae
cielpy <beijiu572@gmail.com>
2019-05-23 00:17:12 +0800

    bump version 0.0.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xgo-spider-log',
-    version='0.0.9',
+    version='0.0.10',
     author='cielpy',
     author_email='beijiu572@gmail.com',
     url='https://github.com/xgoteam/xgo-spider-log',
